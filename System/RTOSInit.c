79618a64c1764e2ab91a091295f0596fed84d4a3
shaoyizhou <1620952301@qq.com>
2023-11-09 15:10:33 +0800

    staticTask

--- System/RTOSInit.c
+++ System/RTOSInit.c
@@ -1,39 +1,74 @@
 #include "RTOSInit.h"
 #include "FreeRTOS.h"
-#include "task.h"
+#include "Kalman.h"
 #include "LED.h"
 #include "OLED.h"
-#include "Kalman.h"
+#include "task.h"
+
 #define START_TASK_STACK_SIZE 128
 #define START_TASK_PRIORITY   1
-TaskHandle_t StartTask_Handler;
+TaskHandle_t StartTask_Handler;                    // 开始任务句柄
+StackType_t startTaskStack[START_TASK_STACK_SIZE]; // 定义开始任务栈空间
+StaticTask_t startTaskTCB;                         // 定义开始任务控制块
+
 #define TASK_1_STACK_SIZE 128
 #define TASK_1_PRIORITY   2
 TaskHandle_t Task1_Handler;
+StackType_t task1_Stack[TASK_1_STACK_SIZE];
+StaticTask_t task1_TCB;
+
 #define TASK_2_STACK_SIZE 128
 #define TASK_2_PRIORITY   3
 TaskHandle_t Task2_Handler;
+StackType_t task2_Stack[TASK_2_STACK_SIZE];
+StaticTask_t task2_TCB;
 
-void RTOS_Init(void) {
-    xTaskCreate((TaskFunction_t)startTask, "StartTask",
-                START_TASK_STACK_SIZE, NULL,
-                START_TASK_PRIORITY, &StartTask_Handler);
+void vApplicationGetIdleTaskMemory(StaticTask_t **ppxIdleTaskTCBBuffer,
+                                   StackType_t **ppxIdleTaskStackBuffer,
+                                   uint32_t *pulIdleTaskStackSize)
+{
+    static StaticTask_t xIdleTaskTCB;
+    static StackType_t uxIdleTaskStack[configMINIMAL_STACK_SIZE];
+    *ppxIdleTaskTCBBuffer   = &xIdleTaskTCB;
+    *ppxIdleTaskStackBuffer = uxIdleTaskStack;
+    *pulIdleTaskStackSize   = configMINIMAL_STACK_SIZE;
+}
+
+void vApplicationGetTimerTaskMemory(StaticTask_t **ppxTimerTaskTCBBuffer,
+                                    StackType_t **ppxTimerTaskStackBuffer,
+                                    uint32_t *pulTimerTaskStackSize)
+{
+    static StaticTask_t xTimerTaskTCB;
+    static StackType_t uxTimerTaskStack[configTIMER_TASK_STACK_DEPTH];
+    *ppxTimerTaskTCBBuffer   = &xTimerTaskTCB;
+    *ppxTimerTaskStackBuffer = uxTimerTaskStack;
+    *pulTimerTaskStackSize   = configTIMER_TASK_STACK_DEPTH;
+}
+
+void RTOS_Init(void)
+{
+    StartTask_Handler = xTaskCreateStatic((TaskFunction_t)startTask, "StarTask",
+                                          START_TASK_STACK_SIZE, NULL, START_TASK_PRIORITY,
+                                          startTaskStack, &startTaskTCB);
     vTaskStartScheduler();
 }
 
-void startTask(void *pvParameters) {
-    taskENTER_CRITICAL();
-    xTaskCreate((TaskFunction_t)task1, "Task1",
-                TASK_1_STACK_SIZE, NULL,
-                TASK_1_PRIORITY, &Task1_Handler);
-    xTaskCreate((TaskFunction_t)task2, "Task2",
-                TASK_2_STACK_SIZE, NULL,
-                TASK_2_PRIORITY, &Task2_Handler);
-    vTaskDelete(NULL);
+void startTask(void *pvParameters)
+{
+    taskENTER_CRITICAL(); // 进入临界区,防止创建任务期间被打断
+    Task1_Handler = xTaskCreateStatic((TaskFunction_t)task1, "Task1",
+                                      TASK_1_STACK_SIZE, NULL, TASK_1_PRIORITY,
+                                      task1_Stack, &task1_TCB);
+
+    Task2_Handler = xTaskCreateStatic((TaskFunction_t)task2, "Task2",
+                                      TASK_2_STACK_SIZE, NULL, TASK_2_PRIORITY,
+                                      task2_Stack, &task2_TCB);
+    vTaskDelete(StartTask_Handler); // 删除开始任务
     taskEXIT_CRITICAL();
 }
 
-void task1(void *pvParameters) {
+void task1(void *pvParameters)
+{
     LED_Init(GPIOC, GPIO_Pin_13);
     while (1) {
         LED_Switch(GPIOC, GPIO_Pin_13);
@@ -41,7 +76,8 @@ void task1(void *pvParameters) {
     }
 }
 
-void task2(void *pvParameters) {
+void task2(void *pvParameters)
+{
     LED_Init(GPIOB, GPIO_Pin_12);
     while (1) {
         LED_Switch(GPIOB, GPIO_Pin_12);
--- User/FreeRTOSConfig.h
+++ User/FreeRTOSConfig.h
@@ -39,33 +39,33 @@
  * See http://www.freertos.org/a00110.html
  *----------------------------------------------------------*/
 
-#define configUSE_PREEMPTION     1
-#define configUSE_IDLE_HOOK      0
-#define configUSE_TICK_HOOK      0
-#define configCPU_CLOCK_HZ       ((unsigned long)72000000)
-#define configTICK_RATE_HZ       ((TickType_t)1000)
-#define configMAX_PRIORITIES     (5)
-#define configMINIMAL_STACK_SIZE ((unsigned short)128)
-#define configTOTAL_HEAP_SIZE    ((size_t)(10 * 1024))
-#define configMAX_TASK_NAME_LEN  (16)
-#define configUSE_TRACE_FACILITY 0
-#define configUSE_16_BIT_TICKS   0
-#define configIDLE_SHOULD_YIELD  1
-#define configSUPPORT_STATIC_ALLOCATION 1
-#define configSUPPORT_DYNAMIC_ALLOCATION 0
-#define xPortPendSVHandler PendSV_Handler
-#define xPortSysTickHandler SysTick_Handler
-#define vPortSVCHandler SVC_Handler
+#define configUSE_PREEMPTION             1
+#define configUSE_IDLE_HOOK              0
+#define configUSE_TICK_HOOK              0
+#define configCPU_CLOCK_HZ               ((unsigned long)72000000)
+#define configTICK_RATE_HZ               ((TickType_t)1000)
+#define configMAX_PRIORITIES             (5)
+#define configMINIMAL_STACK_SIZE         ((unsigned short)128)
+#define configTOTAL_HEAP_SIZE            ((size_t)(10 * 1024))
+#define configMAX_TASK_NAME_LEN          (16)
+#define configUSE_TRACE_FACILITY         0
+#define configUSE_16_BIT_TICKS           0
+#define configIDLE_SHOULD_YIELD          1
+#define configSUPPORT_STATIC_ALLOCATION  1
+#define configSUPPORT_DYNAMIC_ALLOCATION 1
+#define xPortPendSVHandler               PendSV_Handler
+#define xPortSysTickHandler              SysTick_Handler
+#define vPortSVCHandler                  SVC_Handler
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
-#define INCLUDE_vTaskPrioritySet         1
-#define INCLUDE_uxTaskPriorityGet        1
-#define INCLUDE_vTaskDelete              1
-#define INCLUDE_vTaskCleanUpResources    0
-#define INCLUDE_vTaskSuspend             1
-#define INCLUDE_vTaskDelayUntil          1
-#define INCLUDE_vTaskDelay               1
+#define INCLUDE_vTaskPrioritySet      1
+#define INCLUDE_uxTaskPriorityGet     1
+#define INCLUDE_vTaskDelete           1
+#define INCLUDE_vTaskCleanUpResources 0
+#define INCLUDE_vTaskSuspend          1
+#define INCLUDE_vTaskDelayUntil       1
+#define INCLUDE_vTaskDelay            1
 
 /* This is the raw value as per the Cortex-M3 NVIC.  Values can be 255
 (lowest) to 0 (1?) (highest). */
@@ -79,5 +79,11 @@ priority values, 0 to 15.  This must correspond to the
 configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
 NVIC value of 255. */
 #define configLIBRARY_KERNEL_INTERRUPT_PRIORITY 15
+
+/* This is configurations to enable timers */
+#define configUSE_TIMERS             1
+#define configTIMER_TASK_PRIORITY    (configMAX_PRIORITIES - 1)
+#define configTIMER_QUEUE_LENGTH     5
+#define configTIMER_TASK_STACK_DEPTH (configMINIMAL_STACK_SIZE * 2)
 
 #endif /* FREERTOS_CONFIG_H */
